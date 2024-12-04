be52b499eb6ff133c632daa8fec05ae5c64e16d3
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-12-04 15:59:39 +0300

    keycloak https issue

--- scripts/packageconf/scripts/set_hostname.sh
+++ scripts/packageconf/scripts/set_hostname.sh
@@ -5,18 +5,21 @@ echo "Enter the hostname. (default: http://localhost):"
 read -r HOSTNAME
 HOSTNAME="${HOSTNAME:-http://localhost}"
 
+
 # Set the environment variables using the provided hostname
 FE_URL="${HOSTNAME}"
 API_BASE_URL="${HOSTNAME}/api/datafocus-service"
 KEYCLOAK_URL="${HOSTNAME}/auth"
 MINIO_BROWSER_REDIRECT_URL="${HOSTNAME}/minio/"
+KC_HOSTNAME=$(echo "$HOSTNAME" | sed -E 's#^https?://##')
 
 # Display the final values
 echo "Updated values:"
 echo "FE_URL: $FE_URL"
-echo "API_BASE_URL: $API_BASE_URL/api/datafocus-service"
-echo "KEYCLOAK_URL: $KEYCLOAK_URL/auth"
+echo "API_BASE_URL: $API_BASE_URL"
+echo "KEYCLOAK_URL: $KEYCLOAK_URL"
 echo "MINIO_BROWSER_REDIRECT_URL: $MINIO_BROWSER_REDIRECT_URL"
+echo "KC_HOSTNAME (no protocol): $KC_HOSTNAME"
 
 # Ask for confirmation before proceeding
 echo "Do you want to proceed with updating the docker-compose.yml file? (y/n)"
@@ -30,6 +33,7 @@ if [[ "$confirmation" =~ ^[Yy]$ ]]; then
     sed -i "s|API_BASE_URL:.*|API_BASE_URL: \${API_BASE_URL:-$API_BASE_URL}|g" docker-compose.yml
     sed -i "s|KEYCLOAK_URL:.*|KEYCLOAK_URL: \${KEYCLOAK_URL:-$KEYCLOAK_URL}|g" docker-compose.yml
     sed -i "s|MINIO_BROWSER_REDIRECT_URL:.*|MINIO_BROWSER_REDIRECT_URL: \${MINIO_BROWSER_REDIRECT_URL:-$MINIO_BROWSER_REDIRECT_URL}|g" docker-compose.yml
+    sed -i "s|KC_HOSTNAME:.*|KC_HOSTNAME: \${KC_HOSTNAME:-$KC_HOSTNAME}|g" docker-compose.yml
 
     echo "docker-compose.yml file has been updated!"
 else
